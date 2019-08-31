b5d1c75c140ab87a13c5252294826aa7e0f87e67
Robustprogram <robustprogram@gmail.com>
2019-08-31 21:56:44 +1000

    Able to modularly add js code to menu items

--- src/menu-item-scripts.js
+++ src/menu-item-scripts.js
@@ -3,7 +3,13 @@ const { ipcRenderer } = require('electron');
 // Create the list of scripts to run when a menu item is clicked
 module.exports.MENU_ITEM_SCRIPTS = {
   'settings' : () => {
+    const saveSettings = () => {
+      console.log('SAVE!');
+    };
+
     const gtaSAPath = ipcRenderer.sendSync('getSAPath');
     document.querySelector('#sa-directory').value = gtaSAPath;
+
+    document.querySelector('#save-settings').addEventListener('click', saveSettings);
   }
 };

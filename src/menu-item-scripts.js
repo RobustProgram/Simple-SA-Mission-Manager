edb96602a04b2da6803af30fb7cd9de9edd47766
Robustprogram <robustprogram@gmail.com>
2019-09-05 21:15:43 +1000

    1 step closer to being able to launch missions. The mission launcher just needs to replace the files and run the exe

--- src/menu-item-scripts.js
+++ src/menu-item-scripts.js
@@ -1,5 +1,5 @@
 const { app, dialog } = require('electron').remote;
-const JSZip = require("jszip");
+const JSZip = require('jszip');
 const path = require('path');
 const fs = require('fs');
 const Store = require('electron-store');
@@ -12,6 +12,10 @@ const store = new Store({schema});
 
 // Import custom stuff
 const { addNotification } = require('./utilities');
+
+const startMission = fileName => {
+  console.log('Launching mission pack ', fileName);
+}
 
 // Create the list of scripts to run when a menu item is clicked
 module.exports.MENU_ITEM_SCRIPTS = {
@@ -31,13 +35,19 @@ module.exports.MENU_ITEM_SCRIPTS = {
   },
   'missions' : () => {
     const checkMissionFolder = () => {
+      // Here, we are going to do two things, check if a mission folder exists. If it doesn't exist
+      // create the folder.
       const missionDir = app.getPath('userData') + '\\missions';
       if (!fs.existsSync(missionDir)) {
         fs.mkdirSync(missionDir);
       } else {
+        // If the folder exists, we are going to check if there is any mission packs already added.
+        // The following code will refresh the mission display
+        document.querySelector('#mission-display').innerHTML = '';
         for(let file of fs.readdirSync(missionDir)) {
           fs.readFile(missionDir + '\\' + file, async (err, data) => {
             if (err) throw err;
+            // Go through each of the mission packs and add information regarding them to the list
             JSZip.loadAsync(data).then(zip => {
               zip.file('info.json').async('string').then(data => {
                 const missionInfo = JSON.parse(data);
@@ -46,23 +56,33 @@ module.exports.MENU_ITEM_SCRIPTS = {
                 const missionDesc = document.createElement('div');
                 const missionAuthor = document.createElement('div');
                 const missionVersion = document.createElement('div');
+                const missionLaunch = document.createElement('button');
                 let missionDisplay;
 
+                // Set up the elements for the mission info area
                 missionName.innerText = missionInfo.name;
                 missionDesc.innerText = missionInfo.about;
                 missionAuthor.innerText = missionInfo.author;
                 missionVersion.innerText = missionInfo.version;
+                missionLaunch.innerHTML = 'Launch Mission';
                 mission.className = 'mission';
                 missionName.className = 'name';
                 missionDesc.className = 'desc';
                 missionAuthor.className = 'author';
                 missionVersion.className = 'version';
+                missionLaunch.className = 'launch';
 
+                // Add the event listener
+                missionLaunch.addEventListener('click', () => startMission(file));
+
+                // Add the elements of the display to the mission info section
                 mission.appendChild(missionName);
                 mission.appendChild(missionDesc);
                 mission.appendChild(missionAuthor);
                 mission.appendChild(missionVersion);
+                mission.appendChild(missionLaunch);
 
+                // Display it
                 missionDisplay = document.querySelector('#mission-display');
                 if (missionDisplay != undefined) {
                   missionDisplay.appendChild(mission);
